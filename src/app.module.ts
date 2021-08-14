d7c8ccd5fe2fb3aa0b47486c17915d695010e585
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-08-14 00:03:41 -0500

    Lesson 14 - Complete Developer Node.js Dawid Udemy - create config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LocationModule } from './location/location.module';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [LocationModule],
+  imports: [LocationModule, ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 // un modulo es como un bundle
 // nuestra aplicacion puede estar compuesta de varios modulos
